61bc39248a60b7a45c48f891d4369611d47a1c08
NguyenNinh <nguyenvanninh2004pt@gmail.com>
2022-12-06 20:13:30 +0700

    6/12/2022

--- weed1/Ex25KiemTraTamGiac.java
+++ weed1/Ex25KiemTraTamGiac.java
@@ -1,5 +1,20 @@
 package weed1;
 
-public class Ex25KiemTraTamGiac {
+import java.util.Scanner;
 
+public class Ex25KiemTraTamGiac {
+    public static boolean isTriangle(double a,double b,double c){
+        return a<0||b<0||c<0||a*a+b*b<c*c||a*a+c*c<b*b||b*b+c*c<a*a;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        if (!isTriangle(a,b,c)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
 }
