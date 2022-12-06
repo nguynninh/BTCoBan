c3a56c03c18323709032e89ed4ab4de71fb6c632
NguyenNinh <nguyenvanninh2004pt@gmail.com>
2022-12-06 23:57:32 +0700

    6/12/2022

--- weed1/Ex28GiaiPTBac2.java
+++ weed1/Ex28GiaiPTBac2.java
@@ -1,10 +1,12 @@
+package weed1;
+
 import java.util.Scanner;
 
 public class Ex28GiaiPTBac2{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập hệ số bậc 2, a = ");
-        float a = Scanner.nextFloat();
+        float a = scanner.nextFloat();
         System.out.print("Nhập hệ số bậc 1, b = ");
         float b = scanner.nextFloat();
         System.out.print("Nhập hằng số tự do, c = ");
