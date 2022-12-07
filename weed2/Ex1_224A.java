372bfbe1e83c683f01cbad51970c4bd831ea24b3
NguyenNinh <nguyenvanninh2004pt@gmail.com>
2022-12-07 23:44:31 +0700

    7/12/2022

--- weed2/Ex1_224A.java
+++ weed2/Ex1_224A.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Ex1_224A {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double s1 = input.nextInt();
-        double s2 = input.nextInt();
-        double s3 = input.nextInt();
+        Scanner in = new Scanner(System.in);
+        double s1 = in.nextInt();
+        double s2 = in.nextInt();
+        double s3 = in.nextInt();
         double c = s3 * s2 / 2 * s1;
         double a = s3 / c;
         double b = s2 / c;
