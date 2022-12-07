fec20d5241591043c6cf55de10b06fe7f94ce4e3
NguyenNinh <nguyenvanninh2004pt@gmail.com>
2022-12-07 09:10:40 +0700

    7/12/2022

---
+++ weed2/Ex1_224A.java
@@ -0,0 +1,16 @@
+package weed2;
+
+import java.util.Scanner;
+
+public class Ex1_224A {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double s1 = input.nextInt();
+        double s2 = input.nextInt();
+        double s3 = input.nextInt();
+        double c = s3 * s2 / 2 * s1;
+        double a = s3 / c;
+        double b = s2 / c;
+        System.out.println(a + " " + b + " " + c);
+    }
+}
